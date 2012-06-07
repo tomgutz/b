2701d96a5d0f3e783f046c323854217f068fd64b
Tomas Gutierrez <tomgutzjr@gmail.com>
2012-06-07 14:04:12 -0700

    HBS plugin under appDir

--- src/main/script/initialize.js
+++ src/main/script/initialize.js
@@ -4,11 +4,12 @@ require.config({
 		// for require-cs
 		'cs': '../vendor/script/cs',
 		'CoffeeScript': '../vendor/script/CoffeeScript',
+		
 		// for require-handlebars
 		'hbs': '../vendor/script/hbs',
 		'Handlebars' : '../vendor/script/Handlebars',
-		'./hbs/underscore': '../vendor/script/underscore-1.3.3',
-		'./hbs': '../vendor/script/hbs',
+		// './hbs/underscore': '../vendor/script/underscore-1.3.3',
+
 		// libs
 		'jquery': '../vendor/script/jquery-1.7.2',
 		'underscore': '../vendor/script/underscore-1.3.3',
@@ -17,6 +18,7 @@ require.config({
 		'foundation': '../vendor/script/foundation',
 		'chaplin': '../vendor/script/chaplin'
 	},
+	
 	
 	
 	shim: {
@@ -28,11 +30,23 @@ require.config({
         templateExtension : 'hbs',
         // if disableI18n is `true` it won't load locales and the i18n helper
         // won't work as well.
-        disableI18n : false
+        disableI18n : true
     }
 });
 
-require(['jquery',	'underscore',	'backbone',		'handlebars',	'cs!application', 'foundation'],
-function($,			_,				Backbone,		Handlebars,		app) {
-	console.log('test');
+// require(['jquery',	'underscore',	'backbone',		'handlebars',	'cs!application', 'foundation'],
+// function($,			_,				Backbone,		Handlebars,		app) {
+// 	console.log('test');
+// });
+
+
+require(['jquery',	'underscore',	'foundation', 'hbs!template/test'],
+	function($,			_,		foundation,	 test) {
+		console.log('test');
+		var container = document.getElementById('demo-app-container');
+		// Run your template function, and inject it.
+		  container.innerHTML = test({
+		    name : 'HPUI'
+		  });
+
 });
