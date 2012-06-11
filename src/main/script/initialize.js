e7a769cd00ee7d752c0802a19ab73df849ba505e
Tomas Gutierrez <tomgutzjr@gmail.com>
2012-06-11 11:41:19 -0700

    Replaced Underscore with Lodash for overall build.

--- src/main/script/initialize.js
+++ src/main/script/initialize.js
@@ -8,16 +8,14 @@ require.config({
 		// for require-handlebars
 		'hbs': '../vendor/script/hbs',
 		'Handlebars': '../vendor/script/Handlebars',
-		// This AMDfied underscore is exclusively for compiling handlebars plugin
-		'hbs/underscore' : '../vendor/script/hbs/underscore-1.3.3-amd',
 
 		// libs
 		'jquery': '../vendor/script/jquery-1.7.2',
-		'underscore': '../vendor/script/underscore-1.3.3',
+		'underscore': '../vendor/script/lodash-0.3.1',
 		'backbone': '../vendor/script/backbone-0.9.2',
 		'handlebars': '../vendor/script/Handlebars',
 		'foundation': '../vendor/script/foundation',
-		'i18next': '../vendor/script/i18next-1.3.1',
+		// 'i18next': '../vendor/script/i18next-1.3.1',
 		'chaplin': '../vendor/script/chaplin'
 	},
 
